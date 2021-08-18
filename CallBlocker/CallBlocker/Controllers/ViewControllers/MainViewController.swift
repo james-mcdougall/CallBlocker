109be0d2ccdee1cd6fb4640dc1f379ca753549c1
james-mcdougall <jameswilliammcdougall@gmail.com>
2021-08-17 22:54:43 -0500

    Project Setup

---
+++ CallBlocker/CallBlocker/Controllers/ViewControllers/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  CallBlocker
+//
+//  Created by James McDougall on 8/17/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    //MARK: IBOutlets -
+    
+    //MARK: UI Properties -
+    
+    //MARK: Functional Properties -
+
+    //MARK: App Lifecycle -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: UI Configuration -
+    
+    
+    //MARK: Actions -
+}
+
+//MARK: Extensions -
