b3a8300884c7f514e6283ebf7edc12588c0982f2
james-mcdougall <jameswilliammcdougall@gmail.com>
2021-08-17 23:09:33 -0500

    Created main label for launch and main view controllers

---
+++ CallBlocker/CallBlocker/Views/CustomUI/CustomButtons.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtons.swift
+//  CallBlocker
+//
+//  Created by James McDougall on 8/17/21.
+//
+
+import UIKit
+
+struct CustomButtons {
+    
+    func pillButton(button: UIButton) {
+        button.layer.cornerRadius = button.frame.size.height / 2
+        
+    }
+    
+}
