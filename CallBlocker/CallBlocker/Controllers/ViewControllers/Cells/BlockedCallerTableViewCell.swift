08d131d2b579d14ed3067c8347c73a04f6d84841
james-mcdougall <james.mcdougall@goldkey.com>
2021-08-19 09:10:42 -0500

    Created a tableviewcell VC for the blocked callers list

---
+++ CallBlocker/CallBlocker/Controllers/ViewControllers/Cells/BlockedCallerTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BlockedCallerTableViewCell.swift
+//  CallBlocker
+//
+//  Created by James McDougall on 8/19/21.
+//
+
+import UIKit
+
+class BlockedCallerTableViewCell: UITableViewCell {
+
+    
+
+}
